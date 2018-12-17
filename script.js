c06b3fc8681683ce1498e40de71e08817e6d7a6b
Руслан Тиньков <rtinkov@logstream.ru>
2018-12-17 11:18:20 +0300

    Добавление раскрывающегося меню

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.menu-btn').on('click', function(e) {
+        e.prevetDefault;
+        $(this).toggleClass('menu-btn_active');
+        $('.menu-nav').toggleClass('menu-nav_active');
+    });
+
+    $('#menu-nav').on("click", "a", function(e) {
+        e.prevetDefault;
+        var id = $(this).attr('href');
+
+        top = $(id).offset().top;
+
+        $('body,html').animate({scrollTop: top},1500);
+    });
+});

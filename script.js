5036a0a91dbdfde4d681f91930e1212898248f74
Руслан Тиньков <rtinkov@logstream.ru>
2018-12-17 12:53:41 +0300

    Изменения

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         var id = $(this).attr('href');
 
         top = $(id).offset().top;
-
+        
         $('body,html').animate({scrollTop: top},1500);
     });
 });

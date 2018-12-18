d4ca6a6d34b5de5ffa5dce41cfbeb18a89ccb52d
Руслан Тиньков <rtinkov@logstream.ru>
2018-12-18 12:38:51 +0300

    Добавление адаптивности, и некоторых графических элементов

--- script.js
+++ script.js
@@ -3,13 +3,5 @@ $(document).ready(function() {
         e.prevetDefault;
         $(this).toggleClass('menu-btn_active');
         $('.menu-nav').toggleClass('menu-nav_active');
-    });
-
-    $('#menu-nav').on("click", "a", function(e) {
-        e.prevetDefault;
-        var id = $(this).attr('href');
-
-        top = $(id).offset().top;        
-        $('body,html').animate({scrollTop: top},1500);
-    });
+    });   
 });
